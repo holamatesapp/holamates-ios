a26773801f5aac21a00619b32f9269d0e707b30a
Juan Antonio Bernal BellidoJuan Antonio Bernal BellidoJuan Antonio Bernal Bellido <juanantoniobernalbellido@gmail.com>
2026-01-09 21:40:06 +0100

    Initial Commit

---
+++ HolaMates/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HolaMates
+//
+//  Created by Juan Antonio Bernal Bellido on 9/1/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HolaMates/HolaMatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  HolaMatesApp.swift
+//  HolaMates
+//
+//  Created by Juan Antonio Bernal Bellido on 9/1/26.
+//
+
+import SwiftUI
+
+@main
+struct HolaMatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
